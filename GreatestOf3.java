125909b2c7095d19cc36ef807bc6881f98697180
Kavitha0907 <168117493+Kavitha0907@users.noreply.github.com>
2024-05-10 15:41:51 +0530

    GreatestOf3.java

---
+++ GreatestOf3.java
@@ -0,0 +1,15 @@
+
+
+// User function Template for Java
+
+class Solution {
+    int greatestOfThree(int A, int B, int C) {
+        if(A>B&&A>C)
+        return A;
+        else if(B>A&&B>C)
+        return B;
+        else
+        return C;
+        // code here
+    }
+}
